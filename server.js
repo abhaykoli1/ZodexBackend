80727913bc2aacd2227352d73baec48832556892
abhaykoli1 <abhaykoli214@gmail.com>
2025-12-29 03:00:47 +0530

    changes

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "*"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Serve static uploads folder
