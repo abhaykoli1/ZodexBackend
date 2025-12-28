e51eeeb4749699dd8042dd6edbec8ab18bb458ad
abhaykoli1 <abhaykoli214@gmail.com>
2025-12-29 03:04:42 +0530

    changes

--- server.js
+++ server.js
@@ -31,3 +31,8 @@ app.use("/api/v1/blogs", blogRoutes);
 app.use("/api/v1/blogCategory", blogCategory);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
